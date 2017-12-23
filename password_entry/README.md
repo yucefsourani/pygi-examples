b0df1716262dae35b5e684425af82bbc0fcbcb97
youcefsourani <youssef.m.sourani@gmail.com>
2017-12-23 19:44:38 +0200

    Add Password Entry Example

---
+++ password_entry/README.md
@@ -0,0 +1,10 @@
+# Password Entry
+
+
+# Screenshot
+
+![Alt text](https://raw.githubusercontent.com/yucefsourani/gvrecord/master/Screenshot%20from%202017-10-27%2016-36-58.jpg "Screenshot")
+
+
+![Alt text](https://raw.githubusercontent.com/yucefsourani/gvrecord/master/Screenshot%20from%202017-10-24%2017-17-10.jpg "Screenshot")
+
---
+++ password_entry/password_entry.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gdk, GdkPixbuf
+
+class MainWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Password Entry Exmaple")
+        self.set_size_request(200, 100)
+        
+        
+        vbox = Gtk.Box()
+        self.add(vbox)
+        
+        self.icon_show =  GdkPixbuf.Pixbuf.new_from_file("icons/130516.svg")
+        self.icon_hide = GdkPixbuf.Pixbuf.new_from_file("icons/132906.svg")
+        #self.icon_show = GdkPixbuf.Pixbuf.new_from_file_at_size("130516.svg",mainbuttonsizewidth,mainbuttonsizeheight)
+        #self.icon_hide = GdkPixbuf.Pixbuf.new_from_file_at_size("132906.svg",mainbuttonsizewidth,mainbuttonsizeheight)
+
+        
+        self.entry = Gtk.Entry()
+        self.entry.set_visibility(False)
+        vbox.pack_start(self.entry, True, True, 0)
+        self.entry.connect("icon-press",self.on_icon_press)
+        self.entry.props.secondary_icon_tooltip_markup = "<b>Show Password</b>"
+        self.entry.props.secondary_icon_pixbuf = self.icon_show
+
+    def on_icon_press(self, entry,icon_position,event):
+        if event.type == Gdk.EventType.BUTTON_PRESS and event.button==1:
+            if entry.get_visibility():
+                self.entry.set_visibility(False)
+                entry.props.secondary_icon_tooltip_markup = "<b>Show Password</b>"
+                entry.props.secondary_icon_pixbuf = self.icon_show
+            else:
+                self.entry.set_visibility(True)
+                entry.props.secondary_icon_tooltip_markup = "<b>Hide Password</b>"
+                entry.props.secondary_icon_pixbuf = self.icon_hide
+
+win = MainWindow()
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
