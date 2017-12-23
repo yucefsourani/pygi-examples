1c440446094bbc08db3aa0044b21932249d55459
youcefsourani <youssef.m.sourani@gmail.com>
2017-12-23 21:17:47 +0200

    update

--- password_entry/password_entry.py
+++ password_entry/password_entry.py
@@ -10,7 +10,7 @@ class MainWindow(Gtk.Window):
         self.set_size_request(200, 100)
         
         
-        vbox = Gtk.Box()
+        vbox = Gtk.VBox()
         self.add(vbox)
         
         self.icon_show =  GdkPixbuf.Pixbuf.new_from_file("icons/130516.svg")
@@ -19,13 +19,18 @@ class MainWindow(Gtk.Window):
         #self.icon_hide = GdkPixbuf.Pixbuf.new_from_file_at_size("132906.svg",32,32)
 
         
-        entry = Gtk.Entry()
-        entry.set_visibility(False)
-        vbox.pack_start(entry, True, True, 0)
-        entry.connect("icon-press",self.on_icon_press)
-        entry.props.secondary_icon_tooltip_markup = "<b>Show Password</b>"
-        entry.props.secondary_icon_pixbuf = self.icon_show
-
+        self.entry = Gtk.Entry()
+        
+        self.entry.set_visibility(False)
+        vbox.pack_start(self.entry, True, True, 0)
+        self.entry.connect("icon-press",self.on_icon_press)
+        self.entry.props.secondary_icon_tooltip_markup = "<b>Show Password</b>"
+        self.entry.props.secondary_icon_pixbuf = self.icon_show
+        
+        button = Gtk.Button("Print Text")
+        vbox.pack_start(button, True, True, 0)
+        button.connect("clicked",self.on_button_clicked)
+        
     def on_icon_press(self, entry,icon_position,event):
         if event.type == Gdk.EventType.BUTTON_PRESS and event.button==1:
             if entry.get_visibility():
@@ -36,6 +41,11 @@ class MainWindow(Gtk.Window):
                 entry.set_visibility(True)
                 entry.props.secondary_icon_tooltip_markup = "<b>Hide Password</b>"
                 entry.props.secondary_icon_pixbuf = self.icon_hide
+
+    def on_button_clicked(self,button):
+        text = self.entry.get_text()
+        if text:
+            print(self.entry.get_text())
 
 win = MainWindow()
 win.connect("delete-event", Gtk.main_quit)
