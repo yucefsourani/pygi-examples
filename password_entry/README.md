a76702381685da6cdd0efb56624d75e967e8ff3b
youcefsourani <youssef.m.sourani@gmail.com>
2017-12-23 20:23:36 +0200

    update

--- password_entry/README.md
+++ password_entry/README.md
@@ -8,3 +8,7 @@
 
 ![Alt text](https://raw.githubusercontent.com/yucefsourani/pygi-examples/master/password_entry/e2.jpg "Screenshot")
 
+
+# Reference 
+
+https://lazka.github.io/pgi-docs/#Gtk-3.0/classes/Entry.html#Gtk.Entry
--- password_entry/password_entry.py
+++ password_entry/password_entry.py
@@ -19,21 +19,21 @@ class MainWindow(Gtk.Window):
         #self.icon_hide = GdkPixbuf.Pixbuf.new_from_file_at_size("132906.svg",32,32)
 
         
-        self.entry = Gtk.Entry()
-        self.entry.set_visibility(False)
-        vbox.pack_start(self.entry, True, True, 0)
-        self.entry.connect("icon-press",self.on_icon_press)
-        self.entry.props.secondary_icon_tooltip_markup = "<b>Show Password</b>"
-        self.entry.props.secondary_icon_pixbuf = self.icon_show
+        entry = Gtk.Entry()
+        entry.set_visibility(False)
+        vbox.pack_start(entry, True, True, 0)
+        entry.connect("icon-press",self.on_icon_press)
+        entry.props.secondary_icon_tooltip_markup = "<b>Show Password</b>"
+        entry.props.secondary_icon_pixbuf = self.icon_show
 
     def on_icon_press(self, entry,icon_position,event):
         if event.type == Gdk.EventType.BUTTON_PRESS and event.button==1:
             if entry.get_visibility():
-                self.entry.set_visibility(False)
+                entry.set_visibility(False)
                 entry.props.secondary_icon_tooltip_markup = "<b>Show Password</b>"
                 entry.props.secondary_icon_pixbuf = self.icon_show
             else:
-                self.entry.set_visibility(True)
+                entry.set_visibility(True)
                 entry.props.secondary_icon_tooltip_markup = "<b>Hide Password</b>"
                 entry.props.secondary_icon_pixbuf = self.icon_hide
 
